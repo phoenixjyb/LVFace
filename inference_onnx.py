33e3bc7acc3f76db554716e92632e90b93a371b7
youjinghan <youjinghan@bytedance.com>
2025-08-11 22:45:36 +0800

    LVFace Code

--- inference_onnx.py
+++ inference_onnx.py
@@ -1,4 +1,4 @@
-git branch -M main# Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
+# Copyright (c) 2025 Bytedance Ltd. and/or its affiliates
 # SPDX-License-Identifier: MIT
 import requests
 import cv2
